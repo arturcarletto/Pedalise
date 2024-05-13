cb4ca4300fc88bdcfb7922a2fae84d625c52f849
arturcarletto <arturcarletto@gmail.com>
2024-05-12 21:34:33 -0300

    vuetify

--- pedalise-frontend/src/App.vue
+++ pedalise-frontend/src/App.vue
@@ -1,27 +1,11 @@
-<script setup>
-import Navbar from './components/Navbar.vue';
-</script>
-
 <template>
-  <header>
-      <div id="app">
-        <Navbar />
-        <router-view/>
-      </div>
-  </header>
+  <v-app>
+    <v-main>
+      <router-view />
+    </v-main>
+  </v-app>
 </template>
 
-<style scoped>
-header {
-  line-height: 1.5;
-  max-height: 100vh;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-}
-</style>
+<script setup>
+  //
+</script>
---
+++ pedalise-frontend/src/pages/agenda.vue
@@ -0,0 +1,16 @@
+<template>
+  <v-app>
+    <v-content>
+      <Calendar/>
+    </v-content>
+  </v-app>
+</template>
+
+<script setup>
+
+import Calendar from '@/components/Calendar.vue'
+</script>
+
+<style scoped>
+
+</style>
