3c41031f6ee669e362881721f09e728c39347985
arturcarletto <arturcarletto@gmail.com>
2024-05-13 23:39:22 -0300

    events add calendar

--- pedalise-frontend/src/components/v-calendar/usage.vue
+++ pedalise-frontend/src/components/v-calendar/usage.vue
@@ -1,4 +1,24 @@
 <template>
+  <v-container>
+    <v-btn color="primary" @click="openAddEventDialog">Add Event</v-btn>
+    <v-dialog v-model="dialog" persistent max-width="600px">
+      <v-card>
+        <v-card-title>
+          <span class="text-h5">Add New Event</span>
+        </v-card-title>
+        <v-card-text>
+          <v-text-field v-model="newEvent.name" label="Event Name" outlined></v-text-field>
+          <v-text-field v-model="newEvent.start" label="Start Date" type="date" outlined></v-text-field>
+          <v-text-field v-model="newEvent.end" label="End Date" type="date" outlined></v-text-field>
+        </v-card-text>
+        <v-card-actions>
+          <v-spacer></v-spacer>
+          <v-btn color="blue darken-1" text @click="closeDialog">Cancel</v-btn>
+          <v-btn color="blue darken-1" text @click="addEvent">Save</v-btn>
+        </v-card-actions>
+      </v-card>
+    </v-dialog>
+  </v-container>
   <div>
       <v-select
         v-model="type"
@@ -32,6 +52,25 @@
 
 <script>
 import { useDate } from 'vuetify'
+import httpService from '@/api/HttpService'
+import axios from 'axios'
+
+const loading = ref(false)
+
+const events = ref([])
+
+const fetchEvents = async () => {
+  httpService.get('api/v1/events/').then(response => {
+    loading.value = true
+    events.value = response.data;
+    loading.value = false
+  }).catch(error => {
+    console.error('Erro ao buscar eventos:', error);
+    loading.value = false;
+  });
+};
+
+onMounted(fetchEvents)
 
 export default {
   data: () => ({
@@ -44,7 +83,6 @@ export default {
       { title: 'Mon - Fri', value: [1, 2, 3, 4, 5] },
       { title: 'Mon, Wed, Fri', value: [1, 3, 5] },
     ],
-    value: [new Date()],
     events: [],
     colors: [
       'blue',
@@ -65,6 +103,13 @@ export default {
       'Conference',
       'Party',
     ],
+    dialog: false,
+    value: new Date(),
+    newEvent: {
+      name: '',
+      start: '',
+      end: ''
+    }
   }),
   mounted() {
     const adapter = useDate()
@@ -106,6 +151,37 @@ export default {
     rnd(a, b) {
       return Math.floor((b - a + 1) * Math.random()) + a
     },
+    openAddEventDialog() {
+      this.dialog = true;
+    },
+    closeDialog() {
+      this.dialog = false;
+    },
+    addEvent() {
+      const eventToAdd = {
+        ...this.newEvent,
+        color: 'blue' // default color or could be selected by the user
+      };
+      this.events.push(eventToAdd);
+      this.saveEvent(eventToAdd);
+      this.closeDialog();
+      this.resetNewEvent();
+    },
+    resetNewEvent() {
+      this.newEvent = { name: '', start: '', end: '' };
+    },
+    saveEvent(event) {
+      // API call to save the event
+      axios.post('/api/events', event)
+        .then(response => {
+          // Handle response here
+          console.log('Event added:', response.data);
+        })
+        .catch(error => {
+          console.error('There was an error adding the event:', error);
+        });
+    }
+
   },
 }
 </script>
